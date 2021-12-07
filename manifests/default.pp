c8c61bacbbf41661537bbca93ea1629637e80360
Ardy <59075479+rdoix@users.noreply.github.com>
2021-12-07 17:13:07 +0700

    Update default.pp

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,8 +1,12 @@
-#class { 'os_hardening': }
+# Hardening base on your OS
+class { 'os_hardening': }
 
+# Hardening SSH Client
 class { 'ssh_hardening::client': }
+
+#Hardening SSH Server, in this case enable and allowed to use password authentication, and SSH port is 20002
 class { 'ssh_hardening::server':
-  use_pam => true,
+  use_pam => true, 
   pam_auth => true,
   ports => 20002,
 }
