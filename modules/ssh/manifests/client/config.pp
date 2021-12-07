d12f70c0e6ad8634348af0965b2cd4edeced44fd
Ardy <59075479+rdoix@users.noreply.github.com>
2021-12-07 16:15:16 +0700

    Add files via upload

---
+++ modules/ssh/manifests/client/config.pp
@@ -0,0 +1,17 @@
+class ssh::client::config {
+  $options = $ssh::client::merged_options
+  $use_augeas = $ssh::client::use_augeas
+
+  if $use_augeas {
+    create_resources('ssh_config', $options)
+  } else {
+    file { $ssh::params::ssh_config:
+      ensure  => file,
+      owner   => '0',
+      group   => '0',
+      mode    => '0644',
+      content => template("${module_name}/ssh_config.erb"),
+      require => Class['ssh::client::install'],
+    }
+  }
+}
